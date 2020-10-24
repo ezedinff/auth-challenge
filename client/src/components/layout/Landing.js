a6786a79ae4797e4b8995c4230d5a5500baa3b7b
Ezedin <ezedin.fedlu@gmail.com>
2020-10-24 04:03:06 +0300

    fix

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -7,11 +7,6 @@ class Landing extends Component {
       <div style={{ height: "75vh" }} className="container valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
-            <h4>
-              <b>Build</b> a login/auth app with the{" "}
-              <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
-              scratch
-            </h4>
             <p className="flow-text grey-text text-darken-1">
               I'm challenged to do authentication with firebase, react, node and
               mongodb for full stack developer position
